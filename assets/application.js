adb0799f3fba33b137c44a4cca1feb0d50ffba05
Peter Suh <psuh22@gmail.com>
2020-04-23 07:06:38 +1000

    jquery quantity

--- assets/application.js
+++ assets/application.js
@@ -9,10 +9,33 @@ $(document).ready(function() {
       max = $quantity.attr('max') ? parseInt($quantity.attr('max')) : null;
 
       if($button.hasClass('plus')){
-        $quantity.val(quantityValue + 1)
+        $quantity.val(quantityValue + 1).change()
       }
       else if ($button.hasClass('minus')){
-        $quantity.val(quantityValue - 1)
+        $quantity.val(quantityValue - 1).change()
+
       }
+
+    $(document).on('change', '.js-quantity-field', function(event){
+      let
+        $field = $(this),
+        $form = $field.closest('form'),
+        $quantityText = $form.find('.js-quantity-text'),
+        shouldDisableMinus = parseInt(this.value) === 1,
+        $minusbutton = $form.find('.js-quantity-button.minus');
+
+        $quantityText.text(this.value);
+
+      if (shouldDisableMinus){
+        $minusbutton.prop('disabled', true);
+      }
+      else if ($minusbutton.prop('disabled') === true) {
+        $minusbutton.prop('disabled', false);
+      }
+
+    });
+
+
+
   });
 });
